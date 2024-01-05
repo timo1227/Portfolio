c42a1979ae2402d68b32328e40abeed58c3165d0
Tim <tim_1227@aol.com>
2024-01-05 17:10:18 -0500

    refactor: new site layout

---
+++ src/components/mobile-nav.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import * as React from 'react';
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+
+import { Button } from './ui/button';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+
+const MobileNav = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant='ghost'
+          className='mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden'
+        >
+          <svg
+            strokeWidth='1.5'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5'
+          >
+            <path
+              d='M3 5H21'
+              stroke='currentColor'
+              strokeWidth='1.5'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            ></path>
+            <path
+              d='M3 12H21'
+              stroke='currentColor'
+              strokeWidth='1.5'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            ></path>
+            <path
+              d='M3 19H21'
+              stroke='currentColor'
+              strokeWidth='1.5'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            ></path>
+          </svg>
+          <span className='sr-only'>Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='left' className='pr-0 text-2xl'>
+        <MobileLink
+          href='/'
+          className='flex items-center'
+          onOpenChange={setOpen}
+        >
+          <span className='font-bold'>Timothy Estrada</span>
+        </MobileLink>
+        <div className='mt-5 flex flex-col space-y-3'>
+          <MobileLink href='#about' onOpenChange={setOpen}>
+            About
+          </MobileLink>
+          <MobileLink href='#project' onOpenChange={setOpen}>
+            Project
+          </MobileLink>
+          <MobileLink href='mailto:the4@njit.edu' onOpenChange={setOpen}>
+            Contact
+          </MobileLink>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+interface MobileLinkProps extends LinkProps {
+  onOpenChange?: (open: boolean) => void;
+  children: React.ReactNode;
+  className?: string;
+}
+
+function MobileLink({
+  href,
+  onOpenChange,
+  className,
+  children,
+  ...props
+}: MobileLinkProps) {
+  const router = useRouter();
+  return (
+    <Link
+      href={href}
+      onClick={() => {
+        router.push(href.toString());
+        onOpenChange?.(false);
+      }}
+      className={cn(className)}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default MobileNav;
---
+++ src/components/site-footer.tsx
@@ -0,0 +1,24 @@
+import { siteConfig } from '@/config/site';
+
+const SiteFooter = () => {
+  return (
+    <footer className='py-6 md:px-8 md:py-0'>
+      <div className='container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
+        <p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
+          Built by{' '}
+          <a
+            href={siteConfig.links.github}
+            target='_blank'
+            rel='noreferrer'
+            className='font-medium underline underline-offset-4'
+          >
+            timo1227
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default SiteFooter;
