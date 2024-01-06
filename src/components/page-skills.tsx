76a1d76609347951c5ce161d41f2d67070004fdb
Tim <tim_1227@aol.com>
2024-01-05 22:11:02 -0500

    refactor: colors

---
+++ src/components/page-skills.tsx
@@ -0,0 +1,14 @@
+const PageSkills = () => {
+  return (
+    <div className='container relative w-full'>
+      <p className='text-xl md:text-3xl'>
+        Some of the top{' '}
+        <strong>
+          Skills <br /> I have
+        </strong>
+      </p>
+    </div>
+  );
+};
+
+export default PageSkills;
