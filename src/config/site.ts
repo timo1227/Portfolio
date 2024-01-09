9fe1fb43136b59a7dcd43c2910dfff3eb0c97890
Tim <tim_1227@aol.com>
2024-01-08 23:56:41 -0500

    feat: og_image

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: 'Timothy Estrada',
   url: 'https://techtim.dev/',
-  ogImage: 'https://techtim.dev/Memoji.png',
+  ogImage: 'https://techtim.dev/og_image.png',
   description: 'My personal website.',
   links: {
     github: 'https://github.com/timo1227',
